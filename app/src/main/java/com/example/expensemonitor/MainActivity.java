7cafd85051eaaf4c11e94d3efa164aa50332df7c
sudanneupane <neupanesudan123@gmail.com>
2023-11-04 20:37:11 +0545

    Bug Fixes \n-Multiple pressing of reload button doesnot repeatedly adds up the SMS to the screen. Only one instance of messages are displayed.

--- app/src/main/java/com/example/expensemonitor/MainActivity.java
+++ app/src/main/java/com/example/expensemonitor/MainActivity.java
@@ -5,6 +5,7 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
@@ -14,12 +15,14 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import java.time.LocalDate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView display;
+//    private ScrollView scrollView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,25 +35,50 @@ public class MainActivity extends AppCompatActivity {
         ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_SMS}, PackageManager.PERMISSION_GRANTED);
     }
 
+    public String gettoday()
+    {
+        try {
+            LocalDate today = null;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                today = LocalDate.now();
+            }
+            return today.toString();
+        }
+        catch (Exception e )
+        {
+            System.exit(0);
+        }
+        return "";
+    }
+
     public void readSMS(View button) {
         try {
+
             Uri sms = Uri.parse("content://sms");
             Pattern pattern = Pattern.compile("(?i)((debit|debited|credit|credited|withdrawn))");
             Pattern pattern1 = Pattern.compile("(?i)(a/c)");
 //            Pattern pattern = Pattern.compile("(?i)(account)+(?:credit|debit)");
 
 
+
             Cursor cursor = null;
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 cursor = getContentResolver().query(sms, null, null, null);
             }
             cursor.moveToFirst();
-            //
+
+//            long datetime = cursor.getLong(cursor.getColumnIndexOrThrow("date"));
+//            String rangeDate = datetime.split(" ");
+//            //
+
+
+            display.setText(null);
             while (!cursor.isAfterLast()) {
                 // Read the message
                 String message = cursor.getString(12);
                 Matcher matcher = pattern.matcher(message);
                 Matcher matcher1 = pattern1.matcher(message);
+
                 if (matcher.find() && matcher1.find()) {
                     display.append(message + "\n\n");
                 }
@@ -63,13 +91,10 @@ public class MainActivity extends AppCompatActivity {
             }
             //        display.setText(cursor.getString(12));
 
-
             cursor.close();
         } catch (Exception e) {
             Toast.makeText(this, "Error Occurred", Toast.LENGTH_LONG).show();
         }
-
-
 
     }
 }
