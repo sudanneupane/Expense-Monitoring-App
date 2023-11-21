c0a9d74cc9652385fbefd5b9305dd133af5f40b0
sudanneupane <neupanesudan123@gmail.com>
2023-11-22 00:58:58 +0545

    Feature Update: Content are sorted with the dates

--- app/src/main/java/com/example/expensemonitor/MainActivity.java
+++ app/src/main/java/com/example/expensemonitor/MainActivity.java
@@ -5,17 +5,19 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
-import java.time.LocalDate;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -38,21 +40,52 @@ public class MainActivity extends AppCompatActivity {
     public String gettoday()
     {
         try {
-            LocalDate today = null;
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                today = LocalDate.now();
-            }
-            return today.toString();
+            String today;
+            Calendar cal = Calendar.getInstance();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String strDate = sdf.format(cal.getTime());
+//            System.out.println("DATE : " + strDate);
+
+            return  strDate;
+
         }
         catch (Exception e )
         {
-            System.exit(0);
+            Toast.makeText(this, "Error Fetching Date", Toast.LENGTH_LONG).show();
         }
         return "";
     }
 
+    public String getContentDate(@NonNull Cursor position)
+    {
+        long timestampMillis = position.getLong(position.getColumnIndexOrThrow("date" ));
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        Date date = new Date(timestampMillis);
+        String formattedDate = dateFormat.format(date);
+        return formattedDate;
+    }
+
+    public boolean isSameDate(String checker, String checked)
+    {
+        String[] last = checker.split("[- ]");
+        String[] latest = checked.split("[- ]");
+
+        if((last[0].equals(latest[0])) && (last[1].equals(latest[1])) && (last[2].equals(latest[2])) )
+        {
+            return true;
+        }
+        return false;
+
+
+    }
+
     public void readSMS(View button) {
         try {
+            String lastcheckeddate, currentdate;
+            String months[] = {"Jan", "Feb", "Mar", "Apr",
+                    "May", "Jun", "Jul", "Aug", "Sep",
+                    "Oct", "Nov", "Dec"};
+
 
             Uri sms = Uri.parse("content://sms");
             Pattern pattern = Pattern.compile("(?i)((debit|debited|credit|credited|withdrawn))");
@@ -66,10 +99,10 @@ public class MainActivity extends AppCompatActivity {
                 cursor = getContentResolver().query(sms, null, null, null, null);
             //}
             cursor.moveToFirst();
-
 //            long datetime = cursor.getLong(cursor.getColumnIndexOrThrow("date"));
 //            String rangeDate = datetime.split(" ");
 //            //
+            lastcheckeddate = gettoday();
 
 
             display.setText(null);
@@ -78,19 +111,30 @@ public class MainActivity extends AppCompatActivity {
                 String message = cursor.getString(12);
                 Matcher matcher = pattern.matcher(message);
                 Matcher matcher1 = pattern1.matcher(message);
-
                 if (matcher.find() && matcher1.find()) {
+                    currentdate = getContentDate(cursor);
+//                    String date = getContentDate(cursor);
+//                    isSameDate(lastcheckeddate, currentdate);
+                    if(!isSameDate(lastcheckeddate, currentdate))
+                    {
+                        String[] splitdate = currentdate.split("[- ]");
+                        String printDate = months[Integer.parseInt(splitdate[1])] + " " + splitdate[2];
+                        display.append("----------------------------------------------------\n");
+                        display.append( printDate + "\n\n");
+                        lastcheckeddate = currentdate;
+
+                    }
+
+                    // Display the message
                     display.append(message + "\n\n");
                 }
-                // Display the message
-                //            display.append(message + "\n\n");
 
-//                display.append(matcher.find() + "\n\n");
                 // Move to the next message
                 cursor.moveToNext();
             }
             //        display.setText(cursor.getString(12));
 
+            gettoday();
             cursor.close();
         } catch (Exception e) {
             Toast.makeText(this, "Error Occurred", Toast.LENGTH_LONG).show();
